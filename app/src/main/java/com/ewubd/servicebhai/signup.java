fe76b127559bca0781a501952a92f910b3d6b965
gl1tchy_af <fahad.com66.fk@gmail.com>
2022-03-16 02:35:25 +0600

    data validation added

--- app/src/main/java/com/ewubd/servicebhai/signup.java
+++ app/src/main/java/com/ewubd/servicebhai/signup.java
@@ -3,6 +3,8 @@ package com.ewubd.servicebhai;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -28,11 +30,43 @@ public class signup extends AppCompatActivity {
 
     }
     void saveInfo(){
-        System.out.println(name.getText().toString().trim());
-        System.out.println(email.getText().toString().trim());
+        String error = "";
+        String prvname = name.getText().toString().trim();
+        if(prvname.length()>15){
+            error+="Name length is too high";
+        }
+        else{
+            System.out.println("User name "+ name);
+        }
+        String Email = email.getText().toString().trim();
+        if(isValidEmail(Email) ==  false){
+            error+= "Wrong Email";
+            error+= "\n";
+        }
+        else{
+            System.out.println("Email "+ Email);
+        }
         System.out.println(address.getText().toString().trim());
-        System.out.println(phone.getText().toString().trim());
+
+        String Phone = phone.getText().toString().trim();
+        int phoneInt=Integer.parseInt(Phone);
+        if(Phone.length()!= 11 || phoneInt <=0 ){
+            error+= "Phone number got some error ";
+            error+= "\n";
+        }
+        else{
+            System.out.println("Phone "+ phoneInt);
+        }
         System.out.println(password.getText().toString().trim());
         System.out.println(rePassword.getText().toString().trim());
+
+        System.out.println("error"+ error);
+        if(error==""){
+
+        }
+    }
+
+    public static boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
     }
 }

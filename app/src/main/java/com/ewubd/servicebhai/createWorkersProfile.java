0767077e2c5c2dcecd433cb45e31d962c18f38d2
gl1tchy_af <fahad.com66.fk@gmail.com>
2022-03-23 19:09:43 +0600

    expertise added

--- app/src/main/java/com/ewubd/servicebhai/createWorkersProfile.java
+++ app/src/main/java/com/ewubd/servicebhai/createWorkersProfile.java
@@ -7,31 +7,56 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class createWorkersProfile extends AppCompatActivity {
 
     SharedPreferences myPref;
     Button save;
     MyDatabaseHelper DB;
-    EditText expertise, nid;
+    EditText nid;
     int userid;
+    private RadioButton rdElectrician;
+    private RadioButton rdPlumber;
+    private RadioButton rdmechanics;
+    private RadioButton rdOther;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_workers_profile);
         save = findViewById(R.id.saveWorkersProfile);
-        expertise = findViewById(R.id.expertise);
         nid = findViewById(R.id.nidNumber);
+        rdElectrician = findViewById(R.id.rdElectrician);
+        rdPlumber = findViewById(R.id.rdPlumber);
+        rdmechanics = findViewById(R.id.rdmechanics);
+        rdOther = findViewById(R.id.rdOther);
         save.setOnClickListener(v->saveInfo());
         DB= new MyDatabaseHelper(this);
         myPref = getApplicationContext().getSharedPreferences("userId", MODE_PRIVATE);
         userid = myPref.getInt("loggedInID", -1);
     }
     void saveInfo(){
-        String prv_exper = expertise.getText().toString().trim();
+        boolean electricianIsChecked = rdElectrician.isChecked();
+        String checkedOne="";
+        if(electricianIsChecked== true){
+            checkedOne = "Electrician";
+        }
+        boolean plumberIsChecked = rdPlumber.isChecked();
+        if(plumberIsChecked== true){
+            checkedOne = "Plumber";
+        }
+        boolean mechanicsIsChecked = rdmechanics.isChecked();
+        if(mechanicsIsChecked== true){
+            checkedOne = "Mechanics";
+        }
+
+        boolean otherIsChecked = rdOther.isChecked();
+        if(otherIsChecked== true){
+            checkedOne = "Other";
+        }
         String prv_nid = nid.getText().toString().trim();
         int prvNid=Integer.parseInt(prv_nid);
-        Boolean noError = DB.insertWorker(userid, prv_exper, prvNid);
+        Boolean noError = DB.insertWorker(userid, checkedOne, prvNid);
         if(noError==true){
             System.out.println("Data Inserted");
             Intent intent = new Intent(this, workersProfile.class);

0a0dae09286ff1aa48b03b1a8376d831fd315159
Rashik Buksh Rafsan <85847421+rashikbuksh@users.noreply.github.com>
2022-03-25 04:20:53 +0600

    Problem Post Show (2)

--- app/src/main/java/com/ewubd/servicebhai/MyDatabaseHelper.java
+++ app/src/main/java/com/ewubd/servicebhai/MyDatabaseHelper.java
@@ -12,6 +12,7 @@ import androidx.annotation.Nullable;
 public class MyDatabaseHelper extends SQLiteOpenHelper {
     private static final String Database_name= "serviceBhai";
     private static final int Version= 9;
+    private int totalProblem;
 
     private Context context;
 
@@ -106,19 +107,24 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
         if(result==-1) return false;
         else return true;
     }
-    public void getPostedProblems (int id){
+    public String[][] getPostedProblems (){
         SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
         Cursor getProblem = sqLiteDatabase.rawQuery("SELECT p.postid, u.name,p.helptype,p.postdetails FROM problemPosting p,users u  WHERE p.Personid = u.Personid;",null);
         int totalValue = getProblem.getCount();
+        totalProblem = totalValue;
         String postedproblems[][] = new String[totalValue+1][4];
         for(int j=0; j<=totalValue; j++){
             if (getProblem.moveToPosition(j)) {
                 for (int i = 0; i <= 3; i++) {
                     postedproblems[j][i] = getProblem.getString(i);
-                    System.out.println(postedproblems[j][i]);
+                    //System.out.println(postedproblems[j][i]);
                 }
             }
         }
+        return postedproblems;
+    }
+    public int totalProblems(){
+        return totalProblem;
     }
 
 
--- app/src/main/java/com/ewubd/servicebhai/problemShow.java
+++ app/src/main/java/com/ewubd/servicebhai/problemShow.java
@@ -3,8 +3,11 @@ package com.ewubd.servicebhai;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class problemShow extends AppCompatActivity {
 
@@ -20,12 +23,23 @@ public class problemShow extends AppCompatActivity {
         postShow = findViewById(R.id.postShow);
         DB= new MyDatabaseHelper(this);
 
+        String[][] list = DB.getPostedProblems();
+        ArrayList<ArrayList<String>> listoflist = new ArrayList<>();
+
+        for(int j=0; j<DB.totalProblems(); j++){
+            for (int i = 0; i <= 3; i++) {
+
+                //listoflist.get(j).add(list[j][i]);
+
+                System.out.println(list[j][i]);
+            }
+        }
+        //System.out.println(listoflist);
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        DB.getPostedProblems(2);
     }
 }

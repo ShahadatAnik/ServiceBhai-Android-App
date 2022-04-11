c79588d56f324422c930b1a7a93e637a5c6c3381
gl1tchy_af <fahad.com66.fk@gmail.com>
2022-04-12 03:44:46 +0600

    send notification

--- app/src/main/java/com/ewubd/servicebhai/homePage.java
+++ app/src/main/java/com/ewubd/servicebhai/homePage.java
@@ -1,9 +1,15 @@
 package com.ewubd.servicebhai;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -33,6 +39,28 @@ public class homePage extends AppCompatActivity {
         });
         inboxButton = findViewById(R.id.inbox);
         inboxButton.setOnClickListener(v->inboxPro());
+
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("My Notification", "My Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        Intent intent = new Intent(this, inbox.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "My Notification")
+                .setSmallIcon(R.drawable.profile_layout_shaper)
+                .setContentTitle("You have a new message")
+                .setContentText("Someone Wants to get Hire you")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(0, builder.build());
 
     }
     void logout(){
--- app/src/main/java/com/ewubd/servicebhai/homepageForUser.java
+++ app/src/main/java/com/ewubd/servicebhai/homepageForUser.java
@@ -1,9 +1,15 @@
 package com.ewubd.servicebhai;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -38,6 +44,27 @@ public class homepageForUser extends AppCompatActivity {
             Intent intent = new Intent(this, problemShow.class);
             startActivity(intent);
         });
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("My Notification", "My Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        Intent intent = new Intent(this, inbox.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "My Notification")
+                .setSmallIcon(R.drawable.profile_layout_shaper)
+                .setContentTitle("You have a new message")
+                .setContentText("Someone Wants to get your job")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(0, builder.build());
 
     }
 
@@ -82,5 +109,13 @@ public class homepageForUser extends AppCompatActivity {
     void problemPage(){
         Intent intent = new Intent(this, problemPosting.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
     }
 }

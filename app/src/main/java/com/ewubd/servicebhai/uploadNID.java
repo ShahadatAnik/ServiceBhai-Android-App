55ce1410e254c92a8d0397d65adb30331fa11460
gl1tchy_af <fahad.com66.fk@gmail.com>
2022-04-23 03:42:59 +0600

    upload to firebase

--- app/src/main/java/com/ewubd/servicebhai/uploadNID.java
+++ app/src/main/java/com/ewubd/servicebhai/uploadNID.java
@@ -1,6 +1,7 @@
 package com.ewubd.servicebhai;
 
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +9,7 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
@@ -17,10 +19,14 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
 public class uploadNID extends AppCompatActivity {
@@ -100,6 +106,44 @@ public class uploadNID extends AppCompatActivity {
 
     private void uploadFile(){
         if (mImageUri != null){
+            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis() + "." + getFileExtension(mImageUri));
+
+            fileReference.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            mProgressBar.setProgress(0);
+                        }
+                    }, 500);
+
+                    fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            final Uri downloadUrl = uri;
+                            Toast.makeText(uploadNID.this, "Upload Successfull", Toast.LENGTH_LONG).show();
+                            upload upload = new upload(mEditTextFileName.getText().toString().trim(), downloadUrl.toString());
+                            String uploadID = mDatabaseRef.push().getKey();
+                            mDatabaseRef.child(uploadID).setValue(upload);
+                        }
+                    });
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(uploadNID.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                    double progress = (100.0 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                    mProgressBar.setProgress((int)progress);
+                }
+            });
+
 
         }else{
             Toast.makeText(this, "no file selected", Toast.LENGTH_SHORT).show();

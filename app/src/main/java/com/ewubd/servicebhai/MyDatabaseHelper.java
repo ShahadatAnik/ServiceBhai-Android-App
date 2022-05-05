8cdd603322362f56fc57f73485468b1075ec351b
Rashik Buksh Rafsan <85847421+rashikbuksh@users.noreply.github.com>
2022-05-05 22:46:47 +0600

    database conflict

--- app/src/main/java/com/ewubd/servicebhai/MyDatabaseHelper.java
+++ app/src/main/java/com/ewubd/servicebhai/MyDatabaseHelper.java
@@ -11,11 +11,12 @@ import androidx.annotation.Nullable;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 public class MyDatabaseHelper extends SQLiteOpenHelper {
     private static final String Database_name= "serviceBhai";
-    private static final int Version= 20;
+    private static final int Version= 21;
     private int totalProblem;
     public static final String SERVER_URL = "https://servicevai.000webhostapp.com/ServiceBhai/insert.php";
     public static final String FETCH_USER = "https://servicevai.000webhostapp.com/ServiceBhai/selectUser.php";
@@ -30,12 +31,12 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         try{
-            Toast.makeText(context,"Table Created ",Toast.LENGTH_LONG).show();
             db.execSQL("Create TABLE users (Personid INTEGER PRIMARY KEY AUTOINCREMENT,name varchar(50),email varchar(50) UNIQUE,address varchar(100),phone varchar(15),type varchar(15), password varchar(50));");
             db.execSQL("Create TABLE workers (workerid INTEGER PRIMARY KEY AUTOINCREMENT,PersonID INTEGER UNIQUE, expertise varchar(50), NIDNumber INTEGER, bio varchar(100), FOREIGN KEY (PersonID) REFERENCES users(Personid) )");
-            db.execSQL("Create TABLE problemPosting (postid INTEGER PRIMARY KEY AUTOINCREMENT, PersonID INTEGER,title varchar(50), helptype varchar(50), postdetails varchar(100), FOREIGN KEY (PersonID) REFERENCES users(Personid) )");
+            db.execSQL("Create TABLE problemPosting (postid INTEGER PRIMARY KEY AUTOINCREMENT, PersonID INTEGER,title varchar(50), helptype varchar(50), postdetails varchar(100), markAsDone INTEGER DEFAULT 0, FOREIGN KEY (PersonID) REFERENCES users(Personid) )");
             db.execSQL("Create TABLE messages (messageid INTEGER PRIMARY KEY AUTOINCREMENT, fromID INTEGER, toID INTEGER, message varchar(100), datetime date, FOREIGN KEY (fromID) REFERENCES users(Personid), FOREIGN KEY (toID) REFERENCES users(Personid) )");
             db.execSQL("Create TABLE rating (rateid INTEGER PRIMARY KEY AUTOINCREMENT, raterID INTEGER, userID INTEGER, rate INTEGER,review varchar(100), FOREIGN KEY (userID) REFERENCES users(Personid), FOREIGN KEY (raterID) REFERENCES users(Personid) )");
+            db.execSQL("Create TABLE biding (bidingid INTEGER PRIMARY KEY AUTOINCREMENT, postid INTEGER, userID INTEGER, biddingAmount INTEGER,comment varchar(100), FOREIGN KEY (postid) REFERENCES problemPosting(postid), FOREIGN KEY (userID) REFERENCES users(Personid))");
         }
         catch (Exception e){
             Toast.makeText(context,"Error: "+e,Toast.LENGTH_LONG).show();
@@ -49,6 +50,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE if exists problemPosting;");
         db.execSQL("DROP TABLE if exists messages;");
         db.execSQL("DROP TABLE if exists rating;");
+        db.execSQL("DROP TABLE if exists biding;");
         onCreate(db);
     }
     public Boolean insertUser(String name, String email, String address, String phone, String password, String type){
@@ -102,7 +104,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
         Cursor profileInfo = sqLiteDatabase.rawQuery("SELECT * from workers WHERE PersonID='"+id+"';", null);
         if (profileInfo.moveToFirst()) {
             for(int i=0;i<=4;i++){
-                    profile[i] = profileInfo.getString(i);
+                profile[i] = profileInfo.getString(i);
             }
         }
         return profile;
@@ -115,14 +117,20 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
         contentValues.put("helptype", helptype);
         contentValues.put("postdetails", postdetails);
         long result = DB.insert("problemPosting",null,contentValues);
-        if(result==-1) return false;
-        else return true;
+        if(result==-1){
+            Toast.makeText(context,"Please Try Again!!",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else{
+            Toast.makeText(context,"Your Problem Has Been Posted Successfully",Toast.LENGTH_LONG).show();
+            return true;
+        }
     }
 
     public ArrayList<postedProblem> getProblems(){
         ArrayList<postedProblem> arrayList = new ArrayList<>();
         SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
-        Cursor getProblem = sqLiteDatabase.rawQuery("SELECT p.postid,p.personid,p.title, u.name,p.helptype,p.postdetails FROM problemPosting p,users u  WHERE p.Personid = u.Personid;",null);
+        Cursor getProblem = sqLiteDatabase.rawQuery("SELECT p.postid,p.personid,p.title, u.name,p.helptype,p.postdetails,p.markAsDone FROM problemPosting p,users u  WHERE p.Personid = u.Personid;",null);
         while(getProblem.moveToNext()){
             int postid = getProblem.getInt(0);
             int personid = getProblem.getInt(1);
@@ -130,10 +138,13 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
             String name = getProblem.getString(3);
             String helptype = getProblem.getString(4);
             String postdetail = getProblem.getString(5);
-            System.out.println(title);
+            int marksAsDone = getProblem.getInt(6);
+            //System.out.println(title);
+            if(marksAsDone==0){
+                postedProblem postedProblem = new postedProblem(postid,personid,title,name,helptype,postdetail, marksAsDone);
+                arrayList.add(postedProblem);
+            }
 
-            postedProblem postedProblem = new postedProblem(postid,personid,title,name,helptype,postdetail);
-            arrayList.add(postedProblem);
         }
         return arrayList;
     }
@@ -156,8 +167,14 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     public Boolean deletePost(int postid){
         SQLiteDatabase DB = this.getWritableDatabase();
         long result = DB.delete("problemPosting", "postid = ?", new String[]{String.valueOf(postid)});
-        if(result==-1) return false;
-        else return true;
+        if(result==-1){
+            Toast.makeText(context,"Please Try Again!!",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else{
+            Toast.makeText(context,"Your Problem Has Been Deleted Successfully",Toast.LENGTH_LONG).show();
+            return true;
+        }
     }
 
     public String userOrWorker (int personid){
@@ -187,16 +204,23 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     public Boolean sendMessages(int fromID, int toID, String message){
         SQLiteDatabase DB = this.getWritableDatabase();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat();
+        Calendar c = Calendar.getInstance();
+//        Date date = new Date();
         ContentValues contentValues = new ContentValues();
         contentValues.put("fromID", fromID);
         contentValues.put("toID", toID);
         contentValues.put("message", message);
-        contentValues.put("datetime", dateFormat.format(date));
-        long result = DB.insert("messages",null,contentValues);
-        if(result==-1) return false;
-        else return true;
+        contentValues.put("datetime", dateFormat.format(c.getTime()));
+        if(message.equals("")){
+            Toast.makeText(context,"Write Something",Toast.LENGTH_LONG).show();
+            return false;
+        } else{
+            long result = DB.insert("messages",null,contentValues);
+            if(result==-1) return false;
+            else return true;
+        }
+
     }
 
     public ArrayList<inboxArrayList> inboxMessages(int id){
@@ -222,7 +246,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
         while(getWorkers.moveToNext()){
             int fromIDD = getWorkers.getInt(0);
             String messages = getWorkers.getString(1);
-            String date = getWorkers.getString(1);
+            String date = getWorkers.getString(2);
             chatArrayList chatArrayList = new chatArrayList(fromIDD, messages, date);
             arrayList.add(chatArrayList);
         }
@@ -245,8 +269,14 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
         contentValues.put("rate", rate);
         contentValues.put("review", review);
         long result = DB.insert("rating",null,contentValues);
-        if(result==-1) return false;
-        else return true;
+        if(result==-1){
+            Toast.makeText(context,"Please Try Again!!",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else{
+            Toast.makeText(context,"Rating Has Been Posted Successfully",Toast.LENGTH_LONG).show();
+            return true;
+        }
     }
     public ArrayList<workerReviewClass> getReviewbyID(int id){
         ArrayList<workerReviewClass> reviews = new ArrayList<>();
@@ -272,6 +302,100 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
         count.moveToFirst();
         String name= count.getString(0);
         return name;
+    }
+
+    public Boolean insertBidding(int postID,int userID,int biddingAmount, String notes){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("postid", postID);
+        contentValues.put("userID", userID);
+        contentValues.put("biddingAmount", biddingAmount);
+        contentValues.put("comment", notes);
+        long result = DB.insert("biding",null,contentValues);
+        if(result==-1){
+            Toast.makeText(context,"Please Try Again!!",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else{
+            Toast.makeText(context,"Bidding Has Been Posted Successfully",Toast.LENGTH_LONG).show();
+            return true;
+        }
+    }
+
+    public ArrayList<biddingArrayList> bidding(int postID){
+        ArrayList<biddingArrayList> arrayList = new ArrayList<>();
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        Cursor bidding = sqLiteDatabase.rawQuery("select bidingid, postid, userID, biddingAmount, comment  from biding  where postid="+postID+";",null);
+        while(bidding.moveToNext()){
+            int biddingid = bidding.getInt(0);
+            int postid = bidding.getInt(1);
+            int userid = bidding.getInt(2);
+            int biddingamount = bidding.getInt(3);
+            String comment = bidding.getString(4);
+            String biddername = getUserame(userid);
+            int workersID = getWorkersID(userid);
+            biddingArrayList biddingArrayList = new biddingArrayList(biddingid, postid, userid, biddingamount, comment, biddername, workersID);
+            arrayList.add(biddingArrayList);
+        }
+        return arrayList;
+    }
+
+    public boolean markAsDone(int postID){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("markAsDone", 1);
+        long result = DB.update("problemPosting",contentValues, "postid="+postID,null);
+        if(result==-1) return false;
+        else return true;
+    }
+
+    public ArrayList<postedProblem> history(int userID){
+        ArrayList<postedProblem> arrayList = new ArrayList<>();
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        Cursor getProblem = sqLiteDatabase.rawQuery("SELECT p.postid,p.personid,p.title, u.name,p.helptype,p.postdetails,p.markAsDone FROM problemPosting p,users u  WHERE p.Personid = u.Personid;",null);
+        while(getProblem.moveToNext()){
+            int postid = getProblem.getInt(0);
+            int personid = getProblem.getInt(1);
+            String title = getProblem.getString(2);
+            String name = getProblem.getString(3);
+            String helptype = getProblem.getString(4);
+            String postdetail = getProblem.getString(5);
+            int marksAsDone = getProblem.getInt(6);
+            //System.out.println(title);
+            if(marksAsDone==1 && userID == personid){
+                postedProblem postedProblem = new postedProblem(postid,personid,title,name,helptype,postdetail, marksAsDone);
+                arrayList.add(postedProblem);
+            }
+
+        }
+        return arrayList;
+    }
+
+    public int getWorkersID(int userID){
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        Cursor count = sqLiteDatabase.rawQuery("select workerid from workers where PersonID = "+userID+";",null);
+        count.moveToFirst();
+        int workerid= count.getInt(0);
+        return workerid;
+    }
+
+    public boolean hasWorkerProfile(int userID){
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        Cursor count = sqLiteDatabase.rawQuery("select workerid from workers where PersonID = "+userID+";",null);
+        if(count.moveToFirst() == false){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public int getWorkersPersonID(int workersID){
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        Cursor count = sqLiteDatabase.rawQuery("select PersonID from workers where workerid = "+workersID+";",null);
+        count.moveToFirst();
+        int workersPersonID= count.getInt(0);
+        return workersPersonID;
     }
     public ArrayList<String> getEmail(){
         ArrayList<String> emailList = new ArrayList<>();

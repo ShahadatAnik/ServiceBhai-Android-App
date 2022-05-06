ceeefa4eee48f673897639c5706aecd2baa51a80
gl1tchy_af <fahad.com66.fk@gmail.com>
2022-05-07 03:55:38 +0600

    rating remote server

--- app/src/main/java/com/ewubd/servicebhai/notificationcheck.java
+++ app/src/main/java/com/ewubd/servicebhai/notificationcheck.java
@@ -1,16 +1,27 @@
 package com.ewubd.servicebhai;
 
+import android.annotation.SuppressLint;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.IBinder;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class notificationcheck extends Service {
     SharedPreferences myPref;
@@ -28,6 +39,7 @@ public class notificationcheck extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         while (true){
             DB= new MyDatabaseHelper(this);
+            fetchDataMessages();
             myPref = getApplicationContext().getSharedPreferences("userId", MODE_PRIVATE);
             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
                 NotificationChannel channel = new NotificationChannel("My Notification", "My Notification", NotificationManager.IMPORTANCE_DEFAULT);
@@ -67,5 +79,78 @@ public class notificationcheck extends Service {
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         notificationManager.notify(0, builder.build());
+    }
+
+    public void fetchDataMessages(){
+        DB = new MyDatabaseHelper(this);
+        @SuppressLint("StaticFieldLeak")
+        class dbManager extends AsyncTask<String,Void,String>
+        {
+            protected void onPostExecute(String data){
+                try {
+                    JSONArray ja = new JSONArray(data);
+                    JSONObject jo = null;
+
+                    for(int i =0;i<ja.length();i++){
+                        jo=ja.getJSONObject(i);
+                        int messageid = jo.getInt("messageid");
+                        int fromID = jo.getInt("fromID");
+                        int toID = jo.getInt("toID");
+                        String message = jo.getString("message");
+                        String datetime = jo.getString("datetime");
+
+                        System.out.println(messageid+" "+fromID+" "+toID+" "+message+" "+datetime);
+
+                        boolean bool = compareWithMessageRemote(messageid);
+
+                        if(bool){
+                            System.out.println("Data Already Present");
+                        }
+                        else{
+                            Boolean noError = DB.sendMessages(fromID, toID, message);
+                            if(noError){
+                                System.out.println("Data Inserted");
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            protected String doInBackground(String... strings) {
+                try {
+                    URL url = new URL(strings[0]);
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    StringBuffer data = new StringBuffer();
+                    String line;
+
+                    while((line=br.readLine())!=null){
+                        data.append(line+"\n");
+                    }
+                    br.close();
+                    return data.toString();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }
+        dbManager obj =new dbManager();
+        obj.execute(DB.FETCH_MESSAGES);
+    }
+    public boolean compareWithMessageRemote(int postID){
+        ArrayList<Integer> id;
+        DB = new MyDatabaseHelper(this);
+        id = DB.getMessageFromRemote();
+        for(int i=0;i<id.size();i++){
+            System.out.println(postID+" "+id.get(i));
+            if(postID == id.get(i)){
+                return true;
+            }
+        }
+        return false;
     }
 }
